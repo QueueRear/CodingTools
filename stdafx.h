1639f156621182b0c28a33334744d190f8581ff4
QueueRear <49281375+QueueRear@users.noreply.github.com>
2021-03-09 22:23:02 -0800

    Add files via upload

--- stdafx.h
+++ stdafx.h
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <stack>
+#include <assert.h>
 
 using namespace std;
 struct TreeNode
@@ -47,4 +49,53 @@ string integerVectorToString(vector<int> list, int length = -1)
 		result += to_string(number) + ", ";
 	}
 	return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+string stringToString(string input)
+{
+	assert(input.length() >= 2);
+	string result;
+	for (int i = 1; i < input.length() - 1; i++)
+	{
+		char currentChar = input[i];
+		if (input[i] == '\\')
+		{
+			char nextChar = input[i + 1];
+			switch (nextChar)
+			{
+			case '\"':
+				result.push_back('\"');
+				break;
+			case '/':
+				result.push_back('/');
+				break;
+			case '\\':
+				result.push_back('\\');
+				break;
+			case 'b':
+				result.push_back('\b');
+				break;
+			case 'f':
+				result.push_back('\f');
+				break;
+			case 'r':
+				result.push_back('\r');
+				break;
+			case 'n':
+				result.push_back('\n');
+				break;
+			case 't':
+				result.push_back('\t');
+				break;
+			default:
+				break;
+			}
+			i++;
+		}
+		else
+		{
+			result.push_back(currentChar);
+		}
+	}
+	return result;
 }
