433328c7f5cde8454aec00d52fcf749c690e896a
QueueRear <49281375+QueueRear@users.noreply.github.com>
2021-04-13 11:35:31 +0800

    Add files via upload

--- stdafx.h
+++ stdafx.h
@@ -8,6 +8,8 @@
 #include <algorithm>
 #include <stack>
 #include <assert.h>
+#include <queue>
+#include <sstream>
 
 using namespace std;
 struct TreeNode
@@ -98,4 +100,72 @@ string stringToString(string input)
 		}
 	}
 	return result;
+}
+
+void trimLeftTrailingSpaces(string &input)
+{
+	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+					return !isspace(ch);
+				}));
+}
+
+void trimRightTrailingSpaces(string &input)
+{
+	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+					return !isspace(ch);
+				}).base(),
+				input.end());
+}
+
+TreeNode *stringToTreeNode(string input)
+{
+	trimLeftTrailingSpaces(input);
+	trimRightTrailingSpaces(input);
+	input = input.substr(1, input.length() - 2);
+	if (!input.size())
+	{
+		return nullptr;
+	}
+
+	string item;
+	stringstream ss;
+	ss.str(input);
+
+	getline(ss, item, ',');
+	TreeNode *root = new TreeNode(stoi(item));
+	queue<TreeNode *> nodeQueue;
+	nodeQueue.push(root);
+
+	while (true)
+	{
+		TreeNode *node = nodeQueue.front();
+		nodeQueue.pop();
+
+		if (!getline(ss, item, ','))
+		{
+			break;
+		}
+
+		trimLeftTrailingSpaces(item);
+		if (item != "null")
+		{
+			int leftNumber = stoi(item);
+			node->left = new TreeNode(leftNumber);
+			nodeQueue.push(node->left);
+		}
+
+		if (!getline(ss, item, ','))
+		{
+			break;
+		}
+
+		trimLeftTrailingSpaces(item);
+		if (item != "null")
+		{
+			int rightNumber = stoi(item);
+			node->right = new TreeNode(rightNumber);
+			nodeQueue.push(node->right);
+		}
+	}
+	return root;
 }
